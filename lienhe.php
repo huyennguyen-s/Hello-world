094f366de7b8ba15d56dbe9608b365a564f23330
huyennguyen-s <65899582+huyennguyen-s@users.noreply.github.com>
2020-07-11 22:19:03 +0700

    Add files via upload

---
+++ lienhe.php
@@ -0,0 +1,293 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>SOUL LIFE</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+	
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+
+	<aside id="colorlib-breadcrumbs">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 breadcrumbs text-center">
+					<div id="colorlib-logo"><h1>Về chúng tôi</h1> <img src="images/104006040_3018053541645060_4750639676057697116_n.jpg" width="300px" heght="150px" ></div>
+						<p><span><a href="index.php">Trang chủ</a></span> / <span>About me</span></p>
+					</div>
+				</div>
+			</div>
+		</aside>
+
+	<!--	<div id="colorlib-container">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="heading-2">Thông tin  liên hệ </h2>
+						<div class="row contact-info-wrap row-pb-lg">
+							<div class="col-md-3">
+								<p><span><i class="icon-location-2"></i></span> 198 Chùa Bộc <br> Đống Đa, Hà Nội</p>
+							</div>
+							<div class="col-md-3">
+								<p><span><i class="icon-phone3"></i></span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+							</div>
+							<div class="col-md-3">
+								<p><span><i class="icon-paperplane"></i></span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+							</div>
+							<div class="col-md-3">
+								<p><span><i class="icon-globe"></i></span> <a href="#">yourwebsite.com</a></p>
+							</div>
+						</div>
+					</div> -->
+					<div id="colorlib-container">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<img class="img-responsive" src="./images/81c589ede849f0014e134c580b317eee.jpg" alt="">
+					</div>
+					<div class="col-md-6">
+						<p>Hi! Các bạn trẻ đam mê đọc sách. Bạn đang băn khoăn không biết cuốn sách nhìn có vẻ thú vị này sẽ phù hợp với mình ? Bạn muốn đọc được bình luận của những người đã từng sở hữu nó ? Hay ngay sau khi đọc xong cuốn sách rất tâm đắc, bạn phấn khích muốn viết vài dòng review về nó để giới thiệu cho bạn bè mình ? Trang web của chúng tôi sẽ giúp bạn giải đáp những thắc mắc trên bởi vì đó là những nơi có thể đọc được các trích dẫn hay, những review về sách từ những người yêu sách trên toàn thế giới. 
+</p>
+						<blockquote>
+							<strong> Soul Life </strong> không chỉ là nơi dừng chân mà còn là nơi lan tỏa đam mê đọc.
+						</blockquote>
+						<p class="first-letra"> <strong><a href="#">Soul Life </a></strong> Một trang web với giao diện đẹp mắt sẽ thu hút bạn ngay từ lần đầu truy cập. Mới được thành lập trong thời gian gần đây nhưng có những bài viết khá sâu sắc nên nhận được sự quan tâm rất lớn từ cộng đồng. Được sáng lập bởi những bạn trẻ thế hệ 9X có chung niềm đam mê với sách đến từ rất nhiều ngành nghề khác nhau nên các bài viết review về sách rất phong phú, đa dạng. Mục tiêu hoạt động của Soul Life. Là cầu nối gắn kết giữa nhà xuất bản và người đọc, cung cấp địa chỉ tra cứu, tìm kiếm sách. </p>
+						<p>
+							<ul class="colorlib-social-icons">
+								<li><a href="#"><i class="icon-twitter"></i></a></li>
+								<li><a href="#"><i class="icon-facebook"></i></a></li>
+								<li><a href="#"><i class="icon-linkedin"></i></a></li>
+								<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							</ul>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!--			<div class="col-md-12">
+						<br><br><br>
+
+						<div class="row">
+							<div class="col-md-6">
+								<div id="map" class="colorlib-map">
+										<img src="./images/100821105_300426667625203_5368713556701216768_n.jpg" width="630px" height="650px">
+								</div>
+							</div>
+
+							<div class="col-md-6">
+								<h2 class="heading-2">Chúng tôi rất mong nhận ý kiến của bạn</h2>
+								<form  method="POST" action="quan_tri_lien_he.php" enctype="multipart/form-data ">
+									<div class="row form-group">
+										<div class="col-md-6">
+											<label for="fname">Họ và tên</label>
+											<input type="text" name ="txtHoten" id="fname" class="form-control" placeholder="Your name">
+										</div>
+										
+									</div>
+
+									<div class="row form-group">
+										<div class="col-md-12">
+											<label for="email">Email</label>
+											<input type="text" name ="txtEmail" id="email" class="form-control" placeholder="Your email address">
+										</div>
+									</div>
+
+									<div class="row form-group">
+										<div class="col-md-12">
+											<label for="subject">Số điện thoại</label>
+											<input type="text" name ="txtSdt" id="subject" class="form-control" placeholder="Your subject of this message">
+										</div>
+									</div>
+
+
+									<div class="row form-group">
+										<div class="col-md-12">
+											<label for="message">Nội dung liên hệ</label>
+											<textarea name="txtLienhe" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us"></textarea>
+										</div>
+									</div>
+									<div class="form-group">
+										<input type="submit" value="Gửi liên hệ" class="btn btn-primary">
+									</div>
+
+								</form>	
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>-->
+		<div id="colorlib-instagram">
+			<div class="row">
+				<div class="col-md-12 col-md-offset-0 colorlib-heading text-center">
+					<h2>Instagram</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="instagram-entry">
+					<a href="#" class="instagram text-center" style="background-image: url(images/gallery-1.jpg);">
+					</a>
+					<a href="#" class="instagram text-center" style="background-image: url(images/gallery-2.jpg);">
+					</a>
+					<a href="#" class="instagram text-center" style="background-image: url(images/gallery-3.jpg);">
+					</a>
+					<a href="#" class="instagram text-center" style="background-image: url(images/gallery-4.jpg);">
+					</a>
+					<a href="#" class="instagram text-center" style="background-image: url(images/gallery-5.jpg);">
+					</a>
+					<a href="#" class="instagram text-center" style="background-image: url(images/gallery-6.jpg);">
+					</a>
+					<a href="#" class="instagram text-center" style="background-image: url(images/gallery-7.jpg);">
+					</a>
+					<a href="#" class="instagram text-center" style="background-image: url(images/gallery-8.jpg);">
+					</a>
+				</div>
+			</div>
+		</div>
+		<footer id="colorlib-footer" role="contentinfo">
+			<div class="container">
+				<div class="row row-pb-md">
+					<div class="col-md-3">
+						<h2>Navigational</h2>
+						<p>
+							<ul class="colorlib-footer-links">
+								<li><a href="#"><i class="icon-check"></i> Sách cho tuổi trẻ</a></li>
+								<li><a href="#"><i class="icon-check"></i> Sưu tầm đa dạng</a></li>
+								<li><a href="#"><i class="icon-check"></i> Nơi dừng chân</a></li>
+								<li><a href="#"><i class="icon-check"></i> Khám phá mới</a></li>
+								<li><a href="#"><i class="icon-check"></i> Phong cách cuộc sống</a></li>
+								<li><a href="#"><i class="icon-check"></i> Thời trang</a></li>
+								<li><a href="#"><i class="icon-check"></i> Sức khỏe và sắc đẹp</a></li>
+							</ul>
+						</p>
+					</div>
+					<div class="col-md-3">
+					<section class="col_1">
+						<h2>Về chúng tôi</h2>
+						<ul class="list1">
+					<li><a href="#">Sed ut perspiciatis</a></li>
+					<li><a href="#">Unde oiste natus erturor</a></li>
+					<li><a href="#">Sit voluptatem accusa</a></li>
+				</ul>
+					</div>
+					<div class="col-md-3">
+						<section class="col_2">
+				<h3>Follow Us</h3>
+				<ul id="icons">
+					<li><a href="#"><img src="images/icon1.jpg" alt="">Facebook</a></li>
+					<li><a href="#"><img src="images/icon2.jpg" alt="">Twitter</a></li>
+					<li><a href="#"><img src="images/icon3.jpg" alt="">LinkedIn</a></li>
+				</ul>
+        	
+					</div>
+					<div class="col-md-3">
+						</section>
+			<section class="col_1">
+				<h3>Address</h3>
+				<p class="address">
+					<span>
+						Country:Việt Nam <br>
+					</span>
+					<span>
+						City:Hà Nội <br>
+					</span>
+					<span>
+						Email:<a href="mailto:">soullife@mail.com</a> <br>
+					</span>
+				</p>
+       		</section>
+					</div>
+					
+				</div>
+				<!--<div class="row">
+					<div class="col-md-12">
+						<p>
+							<small class="block"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> <<!--
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. </small> 
+							<small class="block">Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+						</p>
+					</div> -->
+				</div>
+			</div>
+		</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Google Map  https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false-->
+	<script src="./images/100821105_300426667625203_5368713556701216768_n.jpg"></script>
+	<script src="js/google_map.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
